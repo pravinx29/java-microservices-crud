489cfd7e9938ef1576206723e416b2af350932b5
Pravin Zugare <pravinzugare227@gmail.com>
2026-03-03 06:00:19 +0530

    Fix YAML formatting and configure Spring Cloud Gateway routes

--- api-gateway/src/main/java/com/ms/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ms/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,10 @@ package com.ms.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(
-	    excludeName = {
-	        "org.springframework.cloud.autoconfigure.LifecycleMvcEndpointAutoConfiguration"
-	    }
-	)
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
